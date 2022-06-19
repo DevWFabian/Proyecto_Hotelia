a0cd3233451baba57024e42aa4df313e21b5204a
DevWFabian <wilsonfabian.cruzandrade@gmail.com>
2022-06-18 22:27:39 -0500

    se modifico el footer y se linio  el la zona inferior asi mismo se arreglo el inicio de encargado   y se esta desarrollando el contenido  deel  encargado

--- proyecto_hotelia/js/barra_desplegable.js
+++ proyecto_hotelia/js/barra_desplegable.js
@@ -4,4 +4,6 @@ btn.addEventListener('click',e => {
     menu.classList.toggle('menu-expanded')
     menu.classList.toggle('menu-collapsed')
     document.querySelector('body').classList.toggle('body-expanded')
+    document.querySelector('footer').classList.toggle('pie_pagina_expanded')
 })
+
