a65517150c45e6d8e66fbadbcc3cc5ce4bfd3e2a
DevWFabian <wilsonfabian.cruzandrade@gmail.com>
2022-06-19 17:39:56 -0500

    se agrgo el header se modifico y se implemento un diseño responsive

--- proyecto_hotelia/js/barra_desplegable.js
+++ proyecto_hotelia/js/barra_desplegable.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector('#menu-btn');
 const menu = document.querySelector('#sidemenu');
+const barra_nav = document.querySelector('#barra_navegacion_superior')
 btn.addEventListener('click',e => {
     menu.classList.toggle('menu-expanded')
     menu.classList.toggle('menu-collapsed')
     document.querySelector('body').classList.toggle('body-expanded')
     document.querySelector('footer').classList.toggle('pie_pagina_expanded')
+    barra_nav.classList.toggle('barra_nav_collapsed')
+    barra_nav.classList.toggle('barra_nav_expanded')
 })
 
