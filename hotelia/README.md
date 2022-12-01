3d9e262e41b3542b51768d00f74fa543bca9c449
DevFabian <wfcruz9@misena.edu.co>
2022-12-01 07:54:54 -0500

    se elimino todo

--- hotelia/README.md
+++
@@ -1,3 +0,0 @@
-# hotelia
-VERSION 1
-![image](https://user-images.githubusercontent.com/107563311/199155056-98403be3-f04a-4933-83a3-3adbee9211ae.png)
--- hotelia/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '@/assets/bootstrap.bundle.min.js'
-import '@/assets/bootstrap.min.css'
-
-createApp(App).use(router).mount('#app')
